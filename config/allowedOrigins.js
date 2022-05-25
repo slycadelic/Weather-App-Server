8984efe2cda536c8353b8b27562b4efd01e09409
Shivam Patel <38561152+slycadelic@users.noreply.github.com>
2022-05-25 17:45:24 +0530

    Added AWS EC2 server

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'ec2-3-108-190-202.ap-south-1.compute.amazonaws.com:3500'
 ];
 
 module.exports = allowedOrigins;
