bd8036e3a2fa5a16979e84a0d5af8c4963192d82
Shivam Patel <38561152+slycadelic@users.noreply.github.com>
2022-05-26 13:04:36 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,8 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://main.d2uwy8h41kqv2z.amplifyapp.com/',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000',
-    'ec2-3-108-190-202.ap-south-1.compute.amazonaws.com:3500'
+    'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
